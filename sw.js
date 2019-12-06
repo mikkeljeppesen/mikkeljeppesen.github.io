b49083853d036dfb58912213522a7a31555f9a7a
Mikkel Ingegaard Jeppesen <3056648@rts-365.dk>
2019-12-06 10:51:43 +0100

    loading function changed, and added css

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
-
-if (workbox) {
-  workbox.routing.registerRoute(new RegExp('/*'), new workbox.strategies.NetworkFirst());
-  console.log('Workbox is loaded!');
-} else {
-  console.log('Workbox is not loaded...');
-}
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"),workbox?(workbox.routing.registerRoute(new RegExp("/*"),new workbox.strategies.NetworkFirst),console.log("Workbox is loaded!")):console.log("Workbox is not loaded...");
